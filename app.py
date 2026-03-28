fbef38a5b30eefd387c857f7854c048921c0b7fe
moniams1234 <moniams1234@gmail.com>
2026-03-28 19:53:59 +0100

    Update app.py
    dodano import plotly.express as px
    import plotly.graph_objects as go

--- app.py
+++ app.py
@@ -8,6 +8,8 @@ from datetime import date, datetime
 
 import pandas as pd
 import streamlit as st
+import plotly.express as px
+import plotly.graph_objects as go
 
 # ---------------- LOGIN ----------------
 def login():
